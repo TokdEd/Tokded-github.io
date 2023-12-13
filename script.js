2d912f22f65e80c132e8cebc38ae4bd24bb42318
TokdEd <134078081+TokdEd@users.noreply.github.com>
2023-12-13 22:45:20 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Function to open the custom modal
+function openCustomModal() {
+  var customModal = document.getElementById('customModal');
+  customModal.style.display = 'block';
+}
+
+// Function to close the custom modal
+function closeCustomModal() {
+  var customModal = document.getElementById('customModal');
+  customModal.style.display = 'none';
+}
